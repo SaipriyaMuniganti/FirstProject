43617c144768cf60692709bf2d37a3007d8775b8
saipriya <saipriya@DESKTOP-JR8NMR7>
2023-09-26 11:16:02 +0530

    Initial commit

--- CoreJavaProject/src/JavaPackage/CheckoutPage.java
+++ CoreJavaProject/src/JavaPackage/CheckoutPage.java
@@ -5,15 +5,15 @@ public class CheckoutPage {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         
-        // Get user input
+        
         System.out.print("Enter the number of items: ");
         int numItems = input.nextInt();
         
-        // Create an array to store the item names and prices
+       
         String[] itemNames = new String[numItems];
         double[] itemPrices = new double[numItems];
         
-        // Get the item names and prices from the user
+       
         for (int i = 0; i < numItems; i++) {
             System.out.print("Enter the name of item " + (i + 1) + ": ");
             itemNames[i] = input.next();
@@ -22,7 +22,7 @@ public class CheckoutPage {
             itemPrices[i] = input.nextDouble();
         }
         
-        // Print the checkout page
+        
         System.out.println("\nCheckout Page");
         System.out.println("-------------");
         
